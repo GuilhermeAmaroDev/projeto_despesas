65384ca028ce7fd6878fa30d6ad1f6065cc06337
Guilherme Amaro <gfernandes182@gmail.com>
2023-06-06 20:28:35 -0300

    Criação das barras do gráfico

--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,26 +1,51 @@
 import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
-
   final String label;
   final double value;
   final double percentage;
 
-  ChartBar({
-    required this.label,
-    required this.value,
-    required this.percentage});
+  ChartBar(
+      {required this.label, required this.value, required this.percentage});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Text('R\$${value.toStringAsFixed(2)}'),
+        FittedBox(
+            child: Text('${value.toStringAsFixed(2)}'),
+        ),
         SizedBox(height: 5),
         Container(
           height: 60,
           width: 10,
-          child: null,
+          child: Stack(
+            /// Permite adicionar um componente dentro do outro
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                /// Construção das barras do gráfico
+                decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.grey,
+                      width: 1.0,
+                    ),
+                    color: Color.fromRGBO(220, 220, 220, 1),
+                    borderRadius: BorderRadius.circular(5)
+
+                    /// para deixar a barra circular
+                    ),
+              ),
+              FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.circular(5)),
+                ),
+              )
+            ],
+          ),
         ),
         SizedBox(height: 5),
         Text(label)
