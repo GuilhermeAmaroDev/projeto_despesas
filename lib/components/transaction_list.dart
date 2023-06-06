17f167a8decab4535d2f14f3bc6a4231b70ac301
Guilherme Amaro <gfernandes182@gmail.com>
2023-06-06 20:50:44 -0300

    Refatoração no design do Card

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -32,44 +32,29 @@ class TransactionList extends StatelessWidget {
         itemBuilder: (ctx, index){
           final tr = transactions[index];
           return Card(
-            child: Row(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(20),
-                  decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Colors.black26,
-                        width: 2,
-                      )),
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    'R\$ ${tr.value}',
-                    style: const TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20,
-                      color: Colors.indigo,
-                    ),
+            elevation: 5,
+            margin: const EdgeInsets.symmetric(
+              vertical: 8,
+              horizontal: 5,
+            ),
+            child: ListTile(
+              leading: CircleAvatar(
+                radius: 30,
+                child: Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: FittedBox(
+                    child: Text('R\$${tr.value}',
+              ),
                   ),
                 ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      tr.title,
-                      style: const TextStyle(
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      DateFormat('d MMM y').format(tr.date),
-                      style: const TextStyle(
-                        color: Colors.grey,
-                      ),
-                    ),
-                  ],
-                )
-              ],
+            ),
+              title: Text(
+                tr.title,
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              subtitle: Text(
+                DateFormat('d MMM y').format(tr.date),
+              ),
             ),
           );
         },
