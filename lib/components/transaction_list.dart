41782c6f5fd5d921b2dd3feace61bf8e6fa041a8
Guilherme Amaro <gfernandes182@gmail.com>
2024-01-10 21:59:02 -0300

    Refatoração Card.

--- lib/components/transaction_list.dart
+++ lib/components/transaction_list.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:projeto_despesas/components/card.dart';
 import '../models/transaction.dart';
 import 'package:intl/intl.dart';
 
@@ -34,37 +35,9 @@ class TransactionList extends StatelessWidget {
             itemCount: transactions.length,
             itemBuilder: (ctx, index) {
               final tr = transactions[index];
-              return Card(
-                elevation: 5,
-                margin: const EdgeInsets.symmetric(
-                  vertical: 8,
-                  horizontal: 5,
-                ),
-                child: ListTile(
-                  leading: CircleAvatar(
-                    radius: 30,
-                    child: Padding(
-                      padding: const EdgeInsets.all(8),
-                      child: FittedBox(
-                        child: Text(
-                          'R\$${tr.value}',
-                        ),
-                      ),
-                    ),
-                  ),
-                  title: Text(
-                    tr.title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  subtitle: Text(
-                    DateFormat('d MMM y').format(tr.date),
-                  ),
-                  trailing: IconButton(
-                    icon: const Icon(Icons.delete),
-                    color: Theme.of(context).colorScheme.error,
-                    onPressed: () => onDelete(tr.id),
-                  ),
-                ),
+              return CardTransaction(
+                tr: tr,
+                onDelete: onDelete,
               );
             },
           );
